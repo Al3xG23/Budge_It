e7e4703fa9b85ef9313fbc257c83635cca65f574
Onyx Vega <mabs.24gl@gmail.com>
2024-04-02 21:47:16 -0400

    Merge branch 'main' of github.com:Al3xG23/Project2 into feature/mariel

--- public/js/budget.js
+++ public/js/budget.js
@@ -1,12 +1,12 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#bill-name').value.trim();
+  const needed_funding = document.querySelector('#bill-funding').value.trim();
+  const description = document.querySelector('#bill-desc').value.trim();
 
   if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+    const response = await fetch(`/api/bills`, {
       method: 'POST',
       body: JSON.stringify({ name, needed_funding, description }),
       headers: {
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create bill');
     }
   }
 };
@@ -26,22 +26,22 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/bills/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete bill');
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-bill-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.bill-list')
   .addEventListener('click', delButtonHandler);
