1adf692de068dee1fc99a712a4f0f5da98a990be
Ilona Ashe <ilonaashe@Ilonas-MacBook-Air.local>
2024-04-08 22:13:02 -0500

    add budgetRoutes to render budget page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,13 @@ const userRoutes = require('./userRoutes');
 const billsRoutes = require('./billsRoutes');
 const incomeRoutes = require('./incomeRoutes');
 const descriptionRoutes = require('./descriptionRoutes'); 
+const budgetRoutes = require('./budgetRoutes'); 
+
 
 router.use('/users', userRoutes);
 router.use('/bills', billsRoutes);
 router.use('/income', incomeRoutes);
 router.use('/', descriptionRoutes);
+router.use('/', budgetRoutes);
 
 module.exports = router;
